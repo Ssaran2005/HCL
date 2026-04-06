695dfc726f997575ff712ad47f947d9d12295db0
Ssaran2005 <saransakthivel2005@gmail.com>
2026-04-06 15:46:02 +0530

    ready to commit

---
+++ Onlineshopping.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Onlineshopping {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        double amount=sc.nextDouble();
+        
+        if(amount>=5000){
+            System.out.print("payable amount="+ amount*0.20);
+        }
+        else if(amount>=2000 && amount<=5000){
+            System.out.print("payable amount="+amount*0.10);
+        }
+        else{
+            System.out.print("No Discount");
+        }
+    }
+}
