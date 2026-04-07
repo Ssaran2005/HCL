1e2d8a58585f7a5422c8a168f6f2226bb714dfbc
Ssaran2005 <saransakthivel2005@gmail.com>
2026-04-07 09:02:34 +0530

    change

--- Fuelcost.java
+++ Fuelcost.java
@@ -2,9 +2,9 @@ import java.util.*;
 public class Fuelcost {
     public static void main(String[]args){
         Scanner sc=new Scanner(System.in);
-        int distance=sc.nextInt();
+        int d=sc.nextInt();
         int mileage=sc.nextInt();
         int fuelprice=sc.nextInt();
-        System.out.print((fuelprice/mileage)*distance);
+        System.out.print((fuelprice/mileage)*d);
     }
 }
